72e70fa966edaf6a9dbbce8232b3dbe53251fcd9
Ariful Islam <arif98741@users.noreply.github.com>
2017-03-20 12:31:50 +0600

    Add files via upload

---
+++ JavaApplication2.java
@@ -0,0 +1,11 @@
+package javaapplication2;
+
+import javax.swing.JOptionPane;
+
+public class JavaApplication2 {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "You have succesfully loggin in", "Dialouge Box", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+}
