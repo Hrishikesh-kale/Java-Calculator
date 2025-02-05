8caa1f14c9b99a52d5bc2ee7ea3cd8d90bf201e3
Hrushikesh Kale <84776928+Hrishikesh-kale@users.noreply.github.com>
2025-02-05 18:18:44 +0530

    Update JavaApplication2.java

--- JavaApplication2.java
+++ JavaApplication2.java
@@ -5,7 +5,7 @@ import javax.swing.JOptionPane;
 public class JavaApplication2 {
 
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "You have succesfully loggin in", "Dialouge Box", JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, "You have succesfully loggin in", "Dialouge Box", JOptionPane.INFORMATION_MESSAGE);  
     }
 
 }
